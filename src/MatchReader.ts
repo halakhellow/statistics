7ad8874c718277655335fab10d8f92cfed3aa578
Hala Alkhellow <halakhellow1996@gmail.com>
2021-05-29 01:33:22 +0300

    add MatchReader file that have the specific data related to football.csv

---
+++ src/MatchReader.ts
@@ -0,0 +1,17 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { dateStringToDate } from "./utilities";
+import { MatchResult } from "./MatchResult";
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class MatchReader extends CsvFileReader<MatchData> {
+  mapRow = (row: string[]): MatchData => [
+    dateStringToDate(row[0]),
+    row[1],
+    row[2],
+    +row[3],
+    +row[4],
+    row[5] as MatchResult,
+    row[6],
+  ];
+}
